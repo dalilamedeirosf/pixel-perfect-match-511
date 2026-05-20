394a97fd2d9074c3096568591fda7a9f759f2eed
Dalila Medeiros <daliladmx@gmail.com>
2026-05-20 08:10:25 -0300

    alt

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -87,7 +87,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600&display=swap",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.cdnfonts.com/css/nexa",
       },
     ],
   }),
